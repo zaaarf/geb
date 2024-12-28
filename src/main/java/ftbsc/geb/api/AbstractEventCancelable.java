5a5bd9260c28cbb42592b33a41f523e673ac7d5c
zaaarf <me@zaaarf.foo>
2024-12-28 01:01:23 +0100

    feat: abstract event cancelable

---
+++ src/main/java/ftbsc/geb/api/AbstractEventCancelable.java
@@ -0,0 +1,19 @@
+package ftbsc.geb.api;
+
+/**
+ * Default implementation of {@link IEventCancelable}.
+ * @since 0.4.1
+ */
+public abstract class AbstractEventCancelable implements IEventCancelable {
+	protected boolean canceled = false;
+
+	@Override
+	public boolean isCanceled() {
+		return this.canceled;
+	}
+
+	@Override
+	public void setCanceled(boolean canceled) {
+		this.canceled = canceled;
+	}
+}
